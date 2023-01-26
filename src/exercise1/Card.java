268d5f9f62e1a0c62ad19e8a6af12320e94a9eab
mdzohairansari <ansarizo@sheridancollege.ca>
2023-01-25 20:00:29 -0500

    ICE 1 committing final changes to branch's local repository

--- src/exercise1/Card.java
+++ src/exercise1/Card.java
@@ -1,12 +1,6 @@
 package exercise1;
 
 /**
- * A class that models playing card Objects. Cards have 
- * a value (note that Ace = 1, Jack = 11, Queen = 12, King = 13)
- * A suit (clubs, hearts, spades, diamonds).
- * There are 52 cards in a deck, no jokers.
- * This code is to be used in ICE1. When you create your own branch,
- * add your name as a modifier.
  * @author dancye
  * @author Paul Bonenfant May 2020
  * @modifier Zohair Ansari Jan 25, 2023
--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -2,10 +2,6 @@ package exercise1;
 import java.util.*;
 
 /**
- * A class that fills a hand of 7 cards with random Card Objects and then asks the user to pick a card.
- * It then searches the array of cards for the match to the user's card. 
- * To be used as starting code in Exercise
- *
  * @author dancye
  * @author Paul Bonenfant Jan 25, 2022
  * @modifier Zohair Ansari Jan 25, 2023
@@ -47,24 +43,10 @@ public class CardTrick {
                 break;
             }
         }
-        
-        // insert code to ask the user for Card value and suit, create their card
-        // and search the hand here. 
-        // Hint: You can ask for values 1 to 10, and then
-        //       11 for jack, 12 for queen, etc. (remember arrays are 0-based though)
-        //       1 for Hearts, 2 for Diamonds, etc. (remember arrays are 0-based though)
-        // 
-        // Then loop through the cards in the array to see if there's a match.
-        
-        // If the guess is successful, invoke the printInfo() method below.
-        
+
     }
 
-    /**
-     * A simple method to print out personal information. Follow the instructions to 
-     * replace this information with your own.
-     * @author Paul Bonenfant Jan 2022
-     */
+    // I'm done!
     private static void printInfo() {
     
         System.out.println("Congratulations, you guessed right!");
@@ -86,7 +68,6 @@ public class CardTrick {
 
         System.out.println();
         
-    
     }
 
 }
